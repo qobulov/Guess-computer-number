2a46bae0a7745a1a24d3ada19e199b6f9634a8b3
Azizbek <azizbekqobulov05@gmail.com>
2024-12-16 14:59:23 +0500

    update

--- Program.cs
+++ Program.cs
@@ -6,36 +6,86 @@ class Program
     {
         Random random = new Random();
         bool playAgain = true;
-        int guess = 0;
-        int attempts = 0;
-        int ComputerNumber = random.Next(1, 101);
-        Console.WriteLine("Computer chose a number between 1 and 100: ");
 
         while (playAgain)
         {
-            guess = Convert.ToInt32(Console.ReadLine());
-            if(guess == ComputerNumber)
+            int guess = 0;
+            int attempts = 0;
+            int computerNumber = random.Next(1, 101);
+            Console.WriteLine("Computer chose a number between 1 and 100: ");
+
+            bool guessedCorrectly = false;
+
+            while (!guessedCorrectly)
             {
-                Console.WriteLine("You win!\nDo you want to play again? (1 for yes, 0 for no)");
-                int ans = Convert.ToInt32(Console.ReadLine());
-                if (ans == 1){
+                Console.Write("Enter your guess: ");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    guess = Convert.ToInt32(input);
+
+                    if (guess < 1 || guess > 100)
+                    {
+                        Console.WriteLine("Please enter a number between 1 and 100.");
+                        continue;
+                    }
+
+                    attempts++;
+
+                    if (guess == computerNumber)
+                    {
+                        Console.WriteLine("You win!");
+                        guessedCorrectly = true;
+                    }
+                    else if (guess > computerNumber)
+                    {
+                        Console.WriteLine("Computer number is lower than {0}. Try again.", guess);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Computer number is higher than {0}. Try again.", guess);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input! Please enter a valid number.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("Number of attempts: {0}", attempts);
+            Console.Write("Do you want to play again? (1 for yes, 0 for no): ");
+            
+            string playAgainInput = Console.ReadLine();
+
+            try
+            {
+                int ans = Convert.ToInt32(playAgainInput);
+                if (ans == 1)
+                {
                     playAgain = true;
-                } else{
+                }
+                else if (ans == 0)
+                {
+                    playAgain = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Exiting the game.");
                     playAgain = false;
                 }
             }
-            else if (guess > ComputerNumber)
+            catch (FormatException)
             {
-                Console.WriteLine("Computer number is lower than {0}. Try again.", guess);
+                Console.WriteLine("Invalid input! Exiting the game.");
+                playAgain = false;
             }
-            else
-            {
-                Console.WriteLine("Computer number is higher than {0}. Try again.", guess);
-            }
-            
-            attempts++;
         }
 
-        Console.WriteLine("Number of attempts: {0}", attempts);
+        Console.WriteLine("Thanks for playing!");
     }
 }
--- obj/Debug/net9.0/TestApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebf4543f955afb6275cbf13c56ce95fb11f5657")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
